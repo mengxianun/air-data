88196e249d4cfe9aad7e8d2dbef8165a462c9882
mengxiangyun <718517636@qq.com>
2018-12-14 14:03:02 +0800

    feat: 添加MariaDB方言

---
+++ jdbc/src/main/java/com/github/mengxianun/jdbc/dialect/MariaDBDialect.java
@@ -0,0 +1,24 @@
+package com.github.mengxianun.jdbc.dialect;
+
+import com.github.mengxianun.core.Dialect;
+import com.google.auto.service.AutoService;
+
+@AutoService(Dialect.class)
+public class MariaDBDialect implements Dialect {
+
+	@Override
+	public String getType() {
+		return "mariadb";
+	}
+
+	@Override
+	public boolean assignDatabase() {
+		return true;
+	}
+
+	@Override
+	public boolean quoteTable() {
+		return true;
+	}
+
+}
