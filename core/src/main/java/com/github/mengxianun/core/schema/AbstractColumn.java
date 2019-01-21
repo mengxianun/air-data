1a2f2d62639b63f2acde3d9f54d466d0bc8eb441
mengxiangyun <718517636@qq.com>
2019-01-21 11:27:54 +0800

    chore: 删除无效类型

--- core/src/main/java/com/github/mengxianun/core/schema/AbstractColumn.java
+++
@@ -1,6 +0,0 @@
-package com.github.mengxianun.core.schema;
-
-@Deprecated
-public abstract class AbstractColumn implements Column {
-
-}
--- core/src/main/java/com/github/mengxianun/core/schema/AbstractTable.java
+++
@@ -1,6 +0,0 @@
-package com.github.mengxianun.core.schema;
-
-@Deprecated
-public abstract class AbstractTable implements Table {
-
-}
